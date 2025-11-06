43a2ac6cf3c197b1ed1ac53da94533fbcf5eaafa
Sandra Chavdarova <sandra.chavdarova@students.finki.ukim.mk>
2025-11-06 11:31:10 +0100

    Exercises

--- ExercisesFirstPartialExam/ShapesApplicationTest.java
+++ ExercisesFirstPartialExam/ShapesApplicationTest.java
@@ -136,12 +136,7 @@ public class ShapesApplicationTest {
         ShapesApplication shapesApplication = new ShapesApplication(10000);
 
         System.out.println("===READING CANVASES AND SHAPES FROM INPUT STREAM===");
-//        try {
         shapesApplication.readCanvases(System.in);
-//        } catch (IrregularCanvasException e) {
-//            System.out.println(e.getMessage());
-//        }
-
         System.out.println("===PRINTING SORTED CANVASES TO OUTPUT STREAM===");
         shapesApplication.printCanvases(System.out);
     }
